50e25f83c0a637d27cb522e239897c3cf8d9318c
“Karnaukhova <masj280906@gmail.com>
2025-10-11 21:26:29 +0300

    [TASK-2-1] +struct for work with temperature

--- alexandra.karnauhova/task-2-1/cmd/service/main.go
+++ alexandra.karnauhova/task-2-1/cmd/service/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"alexandra.karnauhova/task-2-1/internal/temperature"
+)
+
 func main() {
+	var numberDepartment int
+	lol := temperature.RangeTemp{Range: ">=", Temperature: 20}
+
+	_, err := fmt.Scan(&numberDepartment)
+	if err != nil {
+		fmt.Println("Invalid number of departments")
+		return
+	}
+	//пару значений, знак и число
+	//массив этих пар +метод в массиве для подсчета норм знач
+	//вывести все:) ( Т_Т )
 
 }
