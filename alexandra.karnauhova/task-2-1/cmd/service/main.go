715e1f21724ee09fbc9b4e6dec961075853e849e
“Karnaukhova <masj280906@gmail.com>
2025-10-11 21:46:41 +0300

    [TASK-2-1] +func for input department +main

--- alexandra.karnauhova/task-2-1/cmd/service/main.go
+++ alexandra.karnauhova/task-2-1/cmd/service/main.go
@@ -6,17 +6,50 @@ import (
 	"alexandra.karnauhova/task-2-1/internal/temperature"
 )
 
+func inputDataDepartment() {
+	department := temperature.TempManager{
+		Temps:            []temperature.RangeTemp{},
+		IdealTemperature: 0,
+		Max:              100,
+		Min:              0,
+	}
+
+	var numberEmployee int
+
+	_, err := fmt.Scan(&numberEmployee)
+	if err != nil {
+		fmt.Println("Invalid number of employee")
+		return
+	}
+
+	for i := 0; i < numberEmployee; i++ {
+		var signs string
+		var number int
+		_, err := fmt.Scan(&signs, &number)
+		if err != nil {
+			fmt.Println("Invalid temperature")
+			return
+		}
+
+		err = department.AddTemp(signs, number)
+		if err != nil {
+			fmt.Println(-1)
+		} else {
+			fmt.Println(department.IdealTemperature)
+		}
+	}
+}
+
 func main() {
 	var numberDepartment int
-	lol := temperature.RangeTemp{Range: ">=", Temperature: 20}
 
 	_, err := fmt.Scan(&numberDepartment)
 	if err != nil {
 		fmt.Println("Invalid number of departments")
 		return
 	}
-	//пару значений, знак и число
-	//массив этих пар +метод в массиве для подсчета норм знач
-	//вывести все:) ( Т_Т )
 
+	for i := 0; i < numberDepartment; i++ {
+		inputDataDepartment()
+	}
 }
